d674b0a57cae1c302dd529bda86706383cfcdc23
wk-j <somnuk.wk@gmail.com>
2019-03-24 00:48:09 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Assembly Version
+
+[![NuGet](https://img.shields.io/nuget/v/wk.AssemblyVersion.svg)](https://www.nuget.org/packages/wk.AssemblyVersion)
---
+++ src/AssemblyVersion/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.Reflection
+open System.IO
+
+let version asm =
+    let info = asm |> System.IO.FileInfo
+    let version = Assembly.LoadFile(info.FullName).GetName().Version
+    version.ToString()
+
+[<EntryPoint>]
+let main argv =
+    let options = List.ofArray argv
+    match options with
+    | [ asm ] ->
+        if File.Exists asm then
+            printfn "%s" (version asm)
+        0
+    | _ ->
+        printfn "0.0.0.0"
+        -1
